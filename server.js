7085e280a600f88c6f178df2a85f1000bc5a90cd
tespin <tristan.m.espinoza@gmail.com>
2023-05-03 15:54:32 -0700

    feat: add index page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3001;
@@ -9,5 +11,5 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-  res.render('views/index');
+  res.render('index');
 });
