50b5dda344b7b12b9fd8def9889a8d7088e324b5
tespin <tristan.m.espinoza@gmail.com>
2023-05-07 12:10:07 -0700

    feat: get search results based on movie title search

--- public/scripts/searchBar.js
+++ public/scripts/searchBar.js
@@ -1,6 +1,7 @@
 let suggestions = [];
 const searchInput = document.querySelector('.search-input');
-const resultsContainer = document.querySelector('.search-results');
+const resultsContainer = document.querySelector('.autocomplete-results');
+const searchResultsContainer = document.querySelector('.search-results');
 
 window.addEventListener('load', async () => {
   const url = 'https://owen-wilson-wow-api.onrender.com/wows/movies';
@@ -10,10 +11,17 @@ window.addEventListener('load', async () => {
   suggestions = json;
 });
 
+document.body.addEventListener('click', (event) => {
+  if (event.target === searchInput) return;
+  resultsContainer.classList.add('hidden');
+  resultsContainer.innerHTML = '';
+});
+
 searchInput.addEventListener('keyup', (event) => {
   const { key } = event;
 
   switch (key) {
+    case 'Enter':
     case 'Escape':
       event.preventDefault();
       return;
@@ -25,9 +33,13 @@ searchInput.addEventListener('keyup', (event) => {
 searchInput.addEventListener('keydown', (event) => {
   const { key } = event;
 
-  if (key === 'Escape') {
-    resultsContainer.classList.add('hidden');
-    searchInput.value = '';
+  switch (key) {
+    case 'Escape':
+      resultsContainer.classList.add('hidden');
+      searchInput.value = '';
+    case 'Enter':
+    // return;
+    default:
   }
 });
 
@@ -68,8 +80,39 @@ const showResults = () => {
   resultsContainer.classList.remove('hidden');
 };
 
-document.querySelector('form').addEventListener('submit', (event) => {
+document.querySelector('form').addEventListener('submit', async (event) => {
   event.preventDefault();
   const meta = document.querySelector('.search-meta');
   meta.innerHTML = `Search results for: "${searchInput.value}"`;
+  const url =
+    'https://owen-wilson-wow-api.onrender.com/wows/random?results=100';
+  const apiResults = await fetch(url);
+  const json = await apiResults.json();
+  console.log(json[5].movie);
+  console.log(searchInput.value);
+  const results = json.filter((item) => {
+    return item.movie.toLowerCase().startsWith(searchInput.value.toLowerCase());
+  });
+
+  searchResultsContainer.innerHTML = results
+    .slice(0, 3)
+    .map((result, index) => {
+      return `
+      <div class="flex xs:flex-col xs:mt-4">
+        <div class="xs:mb-8">
+          <img
+          src=${result.poster}
+          class="xs:w-96 xs:h-96 object-cover xs:rounded-md xs:shadow-md"
+          />
+          <div class="flex flex-row justify-between xs:w-full xs:mt-2">
+            <p>"${result.full_line}"</p>
+            <p class="text-gray-400">
+              ${result.movie} (${result.year})
+            </p>
+          </div>
+        </div>
+      </div>
+      `;
+    })
+    .join('');
 });
