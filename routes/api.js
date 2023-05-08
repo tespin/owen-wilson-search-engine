4faad5349cb30b803e2bf9c111579105042efc4c
tespin <tristan.m.espinoza@gmail.com>
2023-05-08 09:00:51 -0700

    fix: fetch search results from server

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,19 @@ router.get('/', async (req, res) => {
   const url = 'https://owen-wilson-wow-api.onrender.com/wows/random?results=5';
   const results = await fetch(url);
   const json = await results.json();
-  res.render('index', { results: json });
+  res.render('index', { title: '', randomResults: json });
 });
 
 router.post('/', async (req, res) => {
-  const title = req.body.title;
-  const url = `https://owen-wilson-wow-api.onrender.com/wows/random?movie=${title}`;
-  const results = await fetch(url);
-  const json = await results.json();
-  console.log(json);
+  const input = req.body.title;
+  const url =
+    'https://owen-wilson-wow-api.onrender.com/wows/random?results=100';
+  const apiResults = await fetch(url);
+  const json = await apiResults.json();
+  const results = json.filter((item) => {
+    return item.movie.toLowerCase().startsWith(input.toLowerCase());
+  });
+  res.render('index', { title: input, searchResults: results });
 });
 
 module.exports = router;
