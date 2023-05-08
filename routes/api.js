eaad34f4c4c6664f3ef8ab82a869eae41214c5af
tespin <tristan.m.espinoza@gmail.com>
2023-05-08 11:53:40 -0700

    feat: adding pagination

--- routes/api.js
+++ routes/api.js
@@ -19,4 +19,31 @@ router.post('/', async (req, res) => {
   res.render('index', { title: input, searchResults: results });
 });
 
+router.post('/search/movies/:page', async (req, res) => {
+  const numPerPage = 5;
+  let page = req.params.page || 1;
+  const input = req.body.title;
+
+  const url =
+    'https://owen-wilson-wow-api.onrender.com/wows/random?results=100';
+  const results = await fetch(url);
+  const json = await results.json();
+  const updatedResults = json.filter((item) => {
+    return item.movie.toLowerCase().startsWith(input.toLowerCase());
+  });
+
+  const length = updatedResults.length;
+
+  let start = numPerPage * page - numPerPage;
+  let end = start + numPerPage;
+  let paginatedResults = updatedResults.slice(start, end);
+
+  res.render('search/movies', {
+    results: paginatedResults,
+    current: page,
+    pages: Math.ceil(length / numPerPage),
+    searchTerm: input,
+  });
+});
+
 module.exports = router;
