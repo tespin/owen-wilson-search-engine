7baeeebf2ec4d775d1591ac2d7023eb1b5973de4
tespin <tristan.m.espinoza@gmail.com>
2023-05-09 12:30:56 -0700

    fix: update url path

--- routes/api.js
+++ routes/api.js
@@ -7,19 +7,7 @@ router.get('/', async (req, res) => {
   res.render('index', { title: '', randomResults: json });
 });
 
-// router.post('/', async (req, res) => {
-//   const input = req.body.title;
-//   const url =
-//     'https://owen-wilson-wow-api.onrender.com/wows/random?results=100';
-//   const apiResults = await fetch(url);
-//   const json = await apiResults.json();
-//   const results = json.filter((item) => {
-//     return item.movie.toLowerCase().startsWith(input.toLowerCase());
-//   });
-//   res.render('index', { title: input, searchResults: results });
-// });
-
-router.get('/search/movies/:page', async (req, res) => {
+router.get('/search/:page/movies', async (req, res) => {
   const numPerPage = 5;
   let page = req.params.page || 1;
 
